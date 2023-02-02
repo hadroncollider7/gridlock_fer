58d458bb03477768da0560745df55703cc6cbabd
Hadroncollider <chaulo74@students.rowan.edu>
2023-02-02 17:16:34 -0500

    Create table to store softmax values from FER model inference

--- create_mysql_table.py
+++ create_mysql_table.py
@@ -5,8 +5,7 @@ import yaml
 with open('config.yml','r') as ymlConfigFile:
     config = yaml.safe_load(ymlConfigFile)
 
-# The name of the table to be created
-tableTitle = 'GridlockFER'
+tableTitle = "Gridlock_FER"
 
 try:
     # Connect to the database server.
@@ -16,11 +15,20 @@ try:
         user = config['mysql']['user'],
         password = config['mysql']['password'])
     # Instructions to create the table its columns.
-    mySql_Create_Table_Query = """CREATE TABLE {0:s} (
+    mySql_Create_Table_Query = """CREATE TABLE Gridlock_FER (
                             id INT unsigned NOT NULL,
-                            name VARCHAR(10) NOT NULL,
-                            value INT,
-                            PRIMARY KEY (id)) """.format(tableTitle)
+                            predicted VARCHAR(10) NOT NULL,
+                            value_argmax INT unsigned NOT NULL,
+                            0_neutral_softmax DECIMAL(4,4) NOT NULL,
+                            1_happy_softmax DECIMAL(4,4) NOT NULL,
+                            2_sad_softmax DECIMAL(4,4) NOT NULL,
+                            3_surprise_softmax DECIMAL(4,4) NOT NULL,
+                            4_fear_softmax DECIMAL(4,4) NOT NULL,
+                            5_disgust_softmax DECIMAL(4,4) NOT NULL,
+                            6_anger_softmax DECIMAL(4,4) NOT NULL,
+                            7_contempt_softmax DECIMAL(4,4) NOT NULL,
+                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                            PRIMARY KEY (id)) """
     
     cursor = connection.cursor()
     result = cursor.execute(mySql_Create_Table_Query)       # Execute the table creation instructions.
