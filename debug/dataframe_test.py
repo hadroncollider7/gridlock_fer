93bd3e5d2f91c5ad43a84d53a27915b1e6e6395c
Hadroncollider <chaulo74@students.rowan.edu>
2023-03-30 15:19:21 -0400

    Run test on dataframe object

--- debug/dataframe_test.py
+++ debug/dataframe_test.py
@@ -1,30 +1,49 @@
 import os
 import yaml
+import numpy as np
 import pandas as pd
 
 os.system("cls")
 with open('public_config.yml', 'r') as ymlConfigFile:
     config = yaml.safe_load(ymlConfigFile)
 
-batch_no = config['evaluateInferenceModel']['batch_no']
 column = config['evaluateInferenceModel']['column']
 sheet = config['evaluateInferenceModel']['sheet']
-
-if batch_no == 2:
-    img_path = 'images/batch2/'
-    spreadsheet = 'Batch2_Labels.xlsx'
-elif batch_no == 3:
-    img_path = 'images/batch3/0-995/'
-    spreadsheet = 'Batch3_Labels.xlsx'
-elif batch_no == 4:
-    img_path = 'images/batch4/0-822/'
-    spreadsheet = 'Batch4_Labels.xlsx'
-elif batch_no == 1:
-    img_path = 'images/batch1/0-999/'
-    spreadsheet = 'Batch1_Labels.xlsx'
+spreadsheet = config['evaluateInferenceModel']['spreadsheet']
 
 data = pd.read_excel(io=spreadsheet,
-                     sheet_name=[sheet],
-                     usecols=['Image', column])
+                     usecols=[column],
+                     dtype=np.float16)
 
-print(type(list(data[sheet]['Image'])))
+# Print some information about the dataframe
+# print('datatype: ', type(data))
+# print(f'length: {len(data)}')
+# print(data.loc[:20])
+# print(data.dtypes)
+# print('\n')
+
+# Drop NaN rows
+data.dropna(inplace=True)
+# print('datatype: ', type(data))
+# print(f'length: {len(data)}')
+# print(data.loc[:20])
+# print(data.dtypes)
+
+# Convert dataframe to numpy array
+data = data.to_numpy()
+data = np.squeeze(data)
+print(f'datatype: {type(data)}')
+print(f'shape: {np.shape(data)}')
+print(data[:20])
+
+# Sum the elements of the array
+print('type: ', type(int(np.sum(data))))
+print('sum: ', int(np.sum(data)))
+
+print('len type: ', type(len(data)))
+print('len: ', len(data))
+
+# Calculate the accuracy
+score = int(np.sum(data))/len(data)
+print('score type: ', type(score))
+print(score)
